7431a2343124153ab0fbde63aa8856046dc2116c
Tim Budkas <tnamhay@gmail.com>
2020-10-05 20:51:25 -0600

    added questions to jscript.js

---
+++ script.js
@@ -0,0 +1,44 @@
+quizData=[{
+    question: ' How old is Florin ?',
+    a: '10',
+    b: '17',
+    c: '26',
+    d: '110',
+    correct: 'c'
+},{
+
+        question: 'what is most used programming language  in 2019? ',
+        a: 'Java',
+        b: 'C++',
+        c: 'Python',
+        d: 'JavaScript',
+        correct: 'c'
+},{
+
+    question: 'who is MVP of 2019? in NFL ',
+    a: 'Java',
+    b: 'Mahomes',
+    c: 'Patrick ',
+    d: 'JavaScript',
+    correct: 'b'
+},{
+
+    question: 'what does HTML stands for? ',
+    a: 'Hypertext Markup Language',
+    b: 'Cascading Style Sheet',
+    c: 'Jason Object Notation ',
+    d: 'Helicopter Terminal Motoboats',
+    correct: 'a'
+}{
+
+    question: 'what does HTML stands for? ',
+    a: 'Hypertext Markup Language',
+    b: 'Cascading Style Sheet',
+    c: 'Jason Object Notation ',
+    d: 'Helicopter Terminal Motoboats',
+    correct: 'a'
+}
+
+]
+
+
