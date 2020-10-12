746b4612c000c903e2703fc6112e7610cefd2d7d
Tim Budkas <tnamhay@gmail.com>
2020-10-11 18:54:17 -0600

    debugged , added comment for the functions

--- script.js
+++ script.js
@@ -1,110 +1,105 @@
-const quizData=[{
-    question:' How old is Florin ?',
-    a:'10',
-    b:'17',
-    c:'26',
-    d:'110',
-    correct: 'c'
-},{
-        question: 'what is most used programming language  in 2019? ',
-        a:'Java',
-        b:'C++',
-        c:'Python',
-        d:'JavaScript',
-        correct: 'c'
-},{
-    question: 'who is MVP of 2019? in NFL ',
-    a:'Java',
-    b:'Mahomes',
-    c:'Patrick ',
-    d:'JavaScript',
-    correct: 'b'
-},{
-    question: 'what does HTML stands for? ',
-    a:'Hypertext Markup Language',
-    b:'Cascading Style Sheet',
-    c:'Jason Object Notation ',
-    d:'Helicopter Terminal Motoboats',
-    correct: 'a'
-},{
-    question: 'what does HTML stands for? ',
-    a:'Hypertext Markup Language',
-    b:'Cascading Style Sheet',
-    c:'Jason Object Notation ',
-    d:'Helicopter Terminal Motoboats',
-    correct: 'a'
+//global variables
+var quiz= document.querySelectorAll("#quiz");
+var answerEls=document.querySelectorAll(".answer");
+
+var questionEl=document.querySelector("#question-text");
+var a_text=document.querySelector("#a_text");
+var b_text=document.querySelector("#b_text");
+var c_text=document.querySelector("#c_text");
+var d_text=document.querySelector("#d_text");
+var submitBtn=document.querySelector("#submit");
+
+
+//quiz data
+var quizData=[
+    {
+        question: "How many times has Rafael Nadal won Rolland Garros ?",
+        a:"10",
+        b:"17",
+        c:"13",
+        d:"20",
+        correct: "c",
+    },
+    {
+        question: "what is the capital of Iceland ? ",
+        a:"Amsterdam",
+        b:"Copenhagen",
+        c:"Casbah",
+        d:"Reykjavik",
+        correct: "d "
+    },
+    {
+        question: "who is MVP of 2019? in NFL ",
+        a:"Lamar Jackson",
+        b:"Mahomes ",
+        c:"Patrick Sr ",
+        d:"Aaron Rodgers",
+        correct: "b "
+    },
+    {
+        question: "How painted the Mona Lisa? ",
+        a:"Leonardo Da Vinci ",
+        b:" Vincent Van Gogh",
+        c:" Michel de NostreDame ",
+        d:" Michelangelo",
+        correct: "a"
+    },
+    {
+        question:"In Harry Potter, what is the name of The Weasley' house ? ",
+        a:" Whomping Willow ",
+        b:" Shrieking Shack",
+        c:" The Burrow ",
+        d:" Chamber of Secrets ",
+        correct: "c"
 }];
 
+var currentQuiz = 0;
 
-const answerEls=document.querySelectorAll(".answer");
-const questionEl=document.getElementById("question");
-const a_text=document.getElementById("a_text");
-const b_text=document.getElementById("b_text");
-const c_text=document.getElementById("c_text");
-const d_text=document.getElementById("d_text");
-const submitBtn=document.getElementById("submit");
+var score = 0;
 
-
-let currentQuiz = 0;
-let answer=undefined;
-let score = 0;
 loadQuiz();
-
+// loads quiz Data and starts Quiz
 function loadQuiz(){
-    deselectAnswers();
-    const currentQuizData=quizData[currentQuiz];
-
-    questionEl.innerText=currentQuizData.question;
-    a_text.innerText=currentQuizData.a;
-    b_text.innerText=currentQuizData.b;
-    c_text.innerText=currentQuizData.c;
-    d_text.innerText=currentQuizData.d;
-
-    currentQuestion++;
+    questionEl.innerText = quizData[currentQuiz].question;
+    a_text.innerText=quizData[currentQuiz].a;
+    b_text.innerText=quizData[currentQuiz].b;
+    c_text.innerText=quizData[currentQuiz].c;
+    d_text.innerText=quizData[currentQuiz].d;
+    console.log( questionEl.innerText)
+    //currentQuestion++;
+}
+// checks answer selected 
+function atLeastOneRadio(){
+   var answer;
+   for (var i=0; i< answerEls.length;i++){
+       if ( answerEls[i].checked) {
+       
+        answer = answerEls[i].value;       
+    }
+    return answer;
+   
+   }
 }
 
-function getSelected(){
-   // 
-    let answer=undefined;
-
-    answerEls.forEach((answerEl) => {
-        if(answerEl.checked){
-           answer= answerEl.id;
+// checks user answers , stores score and continues the test 
+submitBtn.addEventListener("click",function(){
+        const answer= atLeastOneRadio();
+        if (answer) {
+            if (answer === quizData[currentQuiz].correct) 
+            {
+                score++;
             }
-        });
-       return answer;
-    }
-
-    function deselectAnswers(){
-    answerEls.forEach((answerEl)=>{
-        answerEl.checked=false;
-        });
-    }
-
-submitBtn.addEventListener('click',()=>{
-  
-   const answer= getSelected();
-   if (answer)
-   {
-       if(answer === quizData[currentQuiz].correct){
-           score++;
-       }
-
-       {
-        currentQuiz++;
-        if ( currentQuiz<quizData.length)
+                currentQuiz++;
+            
+                if (currentQuiz < quizData.length) 
             {
                 loadQuiz();
-            }else{
-            //todo : show results
-                alert("did do U good job?");
+            } else {
+                quiz.innerHTML = `
+                    <h2>You answered correctly at ${score}/${quizData.length} questions.</h2>
+                    
+                    <button onclick="location.reload()">Reload</button>`
+                ;
             }
-       }
-   }
-   
-
-   
-            
-   
-    //loadQuiz();
-});
+        }
+    });
